375b59d1ed0d091d92c0f71d552e244250f96d8c
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2021-09-29 11:47:00 +0200

    Asking for rules added

--- battleship_sem.py
+++ battleship_sem.py
@@ -1,7 +1,20 @@
-#Intro
+# Intro
 
 welcome_msg = "Welcome to the Ultimate Battleship Experience"
 print(welcome_msg)
 print("Who dares to enter the battle? Immediately state your name!")
 player1 = input()
-print("Ok, then, " + player1 +"! Let's begin.")
+print("Ok then, " + player1 +", let's begin.")
+
+# Rules
+rules = "I will hide 10 battleships in total. More on that later."
+print("First things first. Are you familiar with the rules? (y/n)")
+answer_rules = input().lower()
+if answer_rules == "y":
+    print("Looks like we are good to go.")    
+elif answer_rules == "n":
+    print(rules)
+else:
+    print("Come again? Please enter y for yes or n for no.")
+
+# Setting up playing field
