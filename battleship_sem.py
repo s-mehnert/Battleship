7280cd2e2ab83551e15d8bd597056a32dc603b0d
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2021-09-29 11:15:08 +0200

    Intro added

--- battleship_sem.py
+++ battleship_sem.py
@@ -1,2 +1,7 @@
+#Intro
+
 welcome_msg = "Welcome to the Ultimate Battleship Experience"
 print(welcome_msg)
+print("Who dares to enter the battle? Immediately state your name!")
+player1 = input()
+print("Ok, then, " + player1 +"! Let's begin.")
