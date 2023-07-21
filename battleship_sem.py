d4626a3eeedceacd1f077e1a82e1c094baa8e8de
Travis <travissarles@users.noreply.github.com>
2023-07-21 16:04:05 +0200

    Update battleship_sem.py
    I wrote a random line of code which should be deleted.

--- battleship_sem.py
+++ battleship_sem.py
@@ -12,6 +12,8 @@ def select_random_up_down():
     return random.choice(up_down)
 def select_random_left_right():
     return random.choice(left_right)
+def select_random_left_deleteme():
+    return random.choice(left_right)
 
 # Playing field class
 
